5c86470e8c3618f7b2614c0e53dfea3caa966cde
Raphael Fratel <raphael.fratel@gmail.com>
2023-12-11 20:15:43 -0300

    Resolve requisitos 3 e 4

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function criarTabela() {
     }
   }
   criarTabela();
+
+  let corSelecionada = null;
+  let tabela = document.getElementById("pixel-board");
+  
+  function selecionarCor(cor) {
+      if (corSelecionada !== null) {
+          document.getElementById(corSelecionada).classList.remove("selected");
+      }
+      document.getElementById(cor).classList.add("selected");
+      corSelecionada = cor;
+  }
+  
+  function aplicarCor(celula) {
+      if (corSelecionada !== null) {
+          celula.style.backgroundColor = corSelecionada;
+      }
+  }
+  
+  document.getElementById("black").addEventListener("click", function() {
+      selecionarCor("black");
+  });
+  
+  document.getElementById("red").addEventListener("click", function() {
+      selecionarCor("red");
+  });
+  
+  document.getElementById("blue").addEventListener("click", function() {
+      selecionarCor("blue");
+  });
+  
+  document.getElementById("green").addEventListener("click", function() {
+      selecionarCor("green");
+  });
+  
+  // Adiciona um event listener para cada célula na tabela
+  for (let index = 0; index < tabela.rows.length; index++) {
+      for (let index2 = 0; index2 < tabela.rows[index].cells.length; index2++) {
+          tabela.rows[index].cells[index2].addEventListener("click", function() {
+              aplicarCor(this);
+          });
+      }
+  }
+  
