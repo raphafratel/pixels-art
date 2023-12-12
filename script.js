8917a5b2aaf49787060f8a1895aa3d520c73108d
Raphael Fratel <raphael.fratel@gmail.com>
2023-12-12 19:33:38 -0300

    Codigo refatorado e requisito 7 ok

--- script.js
+++ script.js
@@ -1,88 +1,106 @@
-function criarTabela() {
-    let tabela = document.getElementById("pixel-board");
+//Colori a paleta de cores
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black';
+document.getElementsByClassName('color')[1].style.backgroundColor = 'red';
+document.getElementsByClassName('color')[2].style.backgroundColor = 'blue';
+document.getElementsByClassName('color')[3].style.backgroundColor = 'green';
 
-    for (var index = 0; index < 5; index++) {
-      let linha = tabela.insertRow(index);
+//Função para criar tabela - o primeiro for cria 5 linhas e o segundo for cria 5 colunas
+const criarTabela = () => {
+  let tabela = document.getElementById('pixel-board');
 
-      for (var index2 = 0; index2 < 5; index2++) {
-        let celula = linha.insertCell(index2);
-        celula.classList.add("pixel");
-      }
+  for (let index = 0; index < 5; index++) {
+    let linha = tabela.insertRow(index);
+
+    for (let index2 = 0; index2 < 5; index2++) {
+      let celula = linha.insertCell(index2);
+      celula.classList.add('pixel');
     }
   }
-  criarTabela();
+};
+criarTabela();
 
-  let corSelecionada = null;
-  let tabela = document.getElementById("pixel-board");
-  
-  function selecionarCor(cor) {
-      if (corSelecionada !== null) {
-          document.getElementById(corSelecionada).classList.remove("selected");
-      }
-      document.getElementById(cor).classList.add("selected");
-      corSelecionada = cor;
-  }
-  
-  function aplicarCor(celula) {
-      if (corSelecionada !== null) {
-          celula.style.backgroundColor = corSelecionada;
-      }
-  }
-  
-  document.getElementById("black").addEventListener("click", function() {
-      selecionarCor("black");
-  });
-  
-  document.getElementById("red").addEventListener("click", function() {
-      selecionarCor("red");
-  });
-  
-  document.getElementById("blue").addEventListener("click", function() {
-      selecionarCor("blue");
-  });
-  
-  document.getElementById("green").addEventListener("click", function() {
-      selecionarCor("green");
-  });
-  
-  // Adiciona um event listener para cada célula na tabela
-  for (let index = 0; index < tabela.rows.length; index++) {
-      for (let index2 = 0; index2 < tabela.rows[index].cells.length; index2++) {
-          tabela.rows[index].cells[index2].addEventListener("click", function() {
-              aplicarCor(this);
-          });
-      }
-  }
+//Atribui a classe selected para a cor que receber o click do mouse
 
-  function limparQuadro() {
+/* funcao seleciona cor */
+const cores = document.getElementsByClassName('color'); //recuperando os elementos que possuem a classe color.
+
+for (index = 0; index < cores.length; index += 1) {
+  //Cada elemento recebe um eventlistener
+  cores[index].addEventListener('click', (event) => {
+    if (document.getElementsByClassName('selected')[0] !== undefined) {
+      //procura classe selected
+      document
+        .getElementsByClassName('selected')[0]
+        .classList.remove('selected'); //remove a classe selected encontrada
+    }
+    event.target.classList.add('selected'); //cria a classe selected ao elemento que recebeu o clique
+  });
+}
+
+const salvarPixels = () => {
+    const coresSalvas = [];
+    for (let index = 0; index < tabela.rows.length; index += 1) {
+        for (let index2 = 0; index2 < tabela.rows[index].cells.length; index2+=1) {
+          coresSalvas.push([index, index2, tabela.rows[index].cells[index2].style.backgroundColor])
+        }
+    }
+    localStorage.setItem('coresPosicao', JSON.stringify(coresSalvas));
+} 
+
+// Adiciona um event listener para cada célula na tabela
+let tabela = document.getElementById('pixel-board');
+
+for (let index = 0; index < tabela.rows.length; index += 1) {
+  for (let index2 = 0; index2 < tabela.rows[index].cells.length; index2+=1) {
+    tabela.rows[index].cells[index2].addEventListener('click', (event)=> {
+        console.log(event)
+      if (document.getElementsByClassName('selected')[0] !== undefined) {
+        event.target.style.backgroundColor = document.getElementsByClassName('selected')[0].style.backgroundColor;
+        salvarPixels();
+      }
+    });
+  }
+}
+ 
+/*Requisito 5*/
+
+const limparQuadro= ()=> {
     for (let index = 0; index < tabela.rows.length; index++) {
         for (let index2 = 0; index2 < tabela.rows[index].cells.length; index2++) {
             tabela.rows[index].cells[index2].style.backgroundColor = "white";
+            salvarPixels();
         }
     }
 }
 
-  document.getElementById("clear-board").addEventListener("click", function() {
-    limparQuadro();
-});
+document.getElementById("clear-board").addEventListener("click",limparQuadro);
 
-function gerarCorAleatoria() {
+/*Requisito 6*/
+
+const gerarCorAleatoria=()=> {
     const letrasHex = "0123456789ABCDEF";
-    let cor = "#";
-    for (let i = 0; i < 6; i++) {
-        cor += letrasHex[Math.floor(Math.random() * 16)];
+    let corAleatoria = "#";
+    for (let index = 0; index < 6; index+=1) {
+        corAleatoria += letrasHex[Math.floor(Math.random() * 16)];
     }
-    return cor;
+    return corAleatoria;
 }
 
-function aplicarCoresAleatorias() {
-    const paletaCores = ["black", "red", "blue", "green"];
-    for (let i = 0; i < paletaCores.length; i++) {
-        let corAleatoria = gerarCorAleatoria();
-        document.getElementById(paletaCores[i]).style.backgroundColor = corAleatoria;
+document.getElementById("button-random-color").addEventListener("click",()=>{
+    for (let index =0; index < cores.length; index+=1) {
+       cores[index].style.backgroundColor = gerarCorAleatoria();
     }
-}
-
-document.getElementById("button-random-color").addEventListener("click", function() {
-    aplicarCoresAleatorias();
 });
+
+
+/*Requisito 7*/
+
+const recuperaPixel = () => {
+    const posicaoRecuperada = JSON.parse(localStorage.getItem('coresPosicao')) 
+    if (posicaoRecuperada){
+    for (let index = 0; index < posicaoRecuperada.length; index+=1){
+        tabela.rows[posicaoRecuperada[index][0]].cells[posicaoRecuperada[index][1]].style.backgroundColor = posicaoRecuperada[index][2];
+    }
+    }
+}
+recuperaPixel();
